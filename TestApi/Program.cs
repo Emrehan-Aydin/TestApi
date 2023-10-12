c078e502021c64a2b8133c25abf5f64dc856a97f
emreh <emrehan.aydin.tr@gmail.com>
2023-10-13 01:36:37 +0300

    asdsadsa

--- TestApi/Program.cs
+++ TestApi/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 //1000 portunda çalýþ
 builder.WebHost.UseUrls("http://*:1000");
 
