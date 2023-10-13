a1969829ffc32084459009353dac4efed5d21b7c
emreh <emrehan.aydin.tr@gmail.com>
2023-10-13 18:53:31 +0300

    asd

--- TestApi/Controllers/TestController.cs
+++ TestApi/Controllers/TestController.cs
@@ -41,5 +41,17 @@ namespace TestApi.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet("asd")]
+        public IEnumerable<WeatherForecast> GetA()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
