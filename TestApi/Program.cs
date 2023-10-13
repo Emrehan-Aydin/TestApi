34c15cf3173d5c06647f6290c2b8d654649f15c9
emreh <emrehan.aydin.tr@gmail.com>
2023-10-13 16:46:08 +0300

    test jenkins checked

--- TestApi/Program.cs
+++ TestApi/Program.cs
@@ -8,7 +8,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 //1000 portunda çalýþ
-builder.WebHost.UseUrls("http://*:1000");
 var app = builder.Build();
  
 // Configure the HTTP request pipeline.
